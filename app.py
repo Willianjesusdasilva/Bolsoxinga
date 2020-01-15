dad8ea5603202695b851b28e24ec784595ccbf83
Willian Jesus da Silva <46569232+Willianjesusdasilva@users.noreply.github.com>
2020-01-15 14:50:37 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request, render_template
+import re
+from bolsoxinga import functions
+
+app = Flask(__name__)
+
+@app.route('/')
+def static_page():
+    return render_template('index.html')
+
+@app.route('/generate/<string:link>')
+def Func_gerate(link):
+    result = functions.set_xing(link)
+    return result
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
